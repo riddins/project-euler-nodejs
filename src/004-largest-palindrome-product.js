4ebd14399370c7eba640aad5dee0c1775bea6fc1
Wayne Iddins <wayne@wayneiddins.net>
2020-02-22 13:18:49 -0500

    first working solution

--- src/004-largest-palindrome-product.js
+++ src/004-largest-palindrome-product.js
@@ -1,16 +1,26 @@
-function largestPalindromeProduct (numDigits) {
-  let x = parseInt(('9').repeat(numDigits))
-  let y = x
+function largestPalindromeProduct (numDigits, verbose = false) {
+  const max = parseInt(('9').repeat(numDigits))
+  let x = max
+  let y = max
   const min = parseInt('9'.repeat(numDigits - 1))
+  let maxPalindrome = 0
   let product
 
   while (x > min) {
     product = x * y
-    console.log(`The product of ${x} and ${y} is ${product}`)
-    if (isPalindrome(product.toString())) break
-    ({ x, y } = stepMethod(x, y, min))
+    if (verbose) console.log(`The product of ${x} and ${y} is ${product}`)
+    if (isPalindrome(product.toString())) {
+      maxPalindrome = product > maxPalindrome ? product : maxPalindrome
+      y -= 1
+      x = y
+    } else if (x === min + 1) {
+      y -= 1
+      x = y
+    } else {
+      x -= 1
+    }
   }
-  return (x > min) ? (x * y) : -1
+  return maxPalindrome
 }
 
 function isPalindrome (num) {
@@ -18,7 +28,8 @@ function isPalindrome (num) {
   return str.split('').reverse().join('') === str
 }
 
-function stepMethod (x, y, min) {
+/*
+function stepMethodOne (x, y, min) {
   x -= 1
   if (x === min) {
     const numDigits = min.toString().length + 1
@@ -31,6 +42,11 @@ function stepMethod (x, y, min) {
     y: y
   }
 }
+
+function stepMethodTwo (x, y) {
+  return x < y ? { x: x, y: y - 1 } : { x: x - 1, y: y }
+}
+*/
 
 module.exports = {
   largestPalindromeProduct,
